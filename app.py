2077a6b56fc8e0eefbc18d6be88d2fbea4cdd0ef
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-04-07 18:19:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, flash, redirect, url_for
 from example_blueprint import example_blueprint
 app = Flask(__name__)
 app.register_blueprint(example_blueprint)
-app.config['SECRET_KEY'] = 'fjdsbfjksfjdsbfbvjhbdhjbvkjrebjkrvk'
 
   # Code to make replit servers work
 if __name__ == '__main__':
