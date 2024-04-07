3d451b8e2d9af251efe93ba0fb4ff503f9c731fe
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-04-07 21:17:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from example_blueprint import example_blueprint
-import os
 
 app = Flask(__name__)
 app.register_blueprint(example_blueprint)
 
 if __name__ == '__main__':
-    port = os.environ.get('FLASK_PORT') or 8080
-    port = int(port)
-
-    app.run(port=port,host='0.0.0.0')
+  app.run(host='0.0.0.0', port=8080)
