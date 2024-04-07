6d040b345decbcd4e3807d5160467a8cd61567df
shaoyanji <100967396+shaoyanji@users.noreply.github.com>
2024-04-07 20:37:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from example_blueprint import example_blueprint
-app = Flask(__name__)
-app.register_blueprint(example_blueprint)
+from flask import Flask
+import os
 
-  # Code to make replit servers work
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
 if __name__ == '__main__':
-    port = 8081
+    port = os.environ.get('FLASK_PORT') or 8080
     port = int(port)
 
     app.run(port=port,host='0.0.0.0')
